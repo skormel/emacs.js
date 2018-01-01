947a99ee41c6f7b16187a7c3626797f6eb8ccde8
Antonio Delgado <adelgado@chaputronia.com>
2018-01-01 22:55:19 +0100

    [ADD] Some snippets for javascript

--- init.el
+++ init.el
@@ -61,6 +61,7 @@
     php-auto-yasnippets
     php-mode
     projectile
+    pug-mode
     restclient
     restclient-helm
     s
@@ -331,9 +332,12 @@ ARG argument is unkown"
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("2d5c40e709543f156d3dee750cd9ac580a20a371f1b1e1e3ecbef2b895cf0cd2" "be5b03913a1aaa3709d731e1fcfd4f162db6ca512df9196c8d4693538fa50b86" default)))
  '(package-selected-packages
    (quote
-    (magit-svn zygospore yasnippet ws-butler volatile-highlights undo-tree tide tern-auto-complete smartparens scss-mode sass-mode projectile markdown-mode magit json-mode js2-mode iedit helm-css-scss ggtags exec-path-from-shell editorconfig duplicate-thing dtrt-indent company comment-dwim-2 color-theme-modern clean-aindent-mode browse-kill-ring anzu 2048-game))))
+    (typing protobuf-mode magit-svn zygospore yasnippet ws-butler volatile-highlights undo-tree tide tern-auto-complete smartparens scss-mode sass-mode projectile markdown-mode magit json-mode js2-mode iedit helm-css-scss ggtags exec-path-from-shell editorconfig duplicate-thing dtrt-indent company comment-dwim-2 color-theme-modern clean-aindent-mode browse-kill-ring anzu 2048-game))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
